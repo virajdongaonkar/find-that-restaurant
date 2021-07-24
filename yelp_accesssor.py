8fc3e0e2c19512e6697e1b68718712fcc774e0fe
Viraj Dongaonkar <virajd@utexas.edu>
2021-07-23 23:59:52 -0400

    Working endpoint connection

--- yelp_accesssor.py
+++ yelp_accesssor.py
@@ -1,15 +1,27 @@
 import json
-
-#from requests.auth import HTTPBasicAuth
-#import requests
+import requests
 
 reader = open('credentials.txt', 'r')
 credentials = reader.readlines()
-
-url = 'https://api.yelp.com/v3'
-
-auth = 'Bearer ' + credentials[1]
-print(auth)
-headers = {'Content-Type': 'application/json', 'Authorization': auth}
-
+API_KEY = credentials[1]
+headers = {
+        'Authorization': 'Bearer %s' % API_KEY,
+}
 reader.close()
+  
+# api-endpoint
+URL = "https://api.yelp.com/v3/businesses/search"
+
+location = "78705"
+
+
+# defining parameters to be sent to the API
+PARAMS = {'location':location}
+  
+# sending get request 
+r = requests.get(url = URL, headers=headers, params = PARAMS)
+  
+# extracting data
+data = r.json()
+
+print(data)
